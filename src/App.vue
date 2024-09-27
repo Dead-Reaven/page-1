3bbcbbb0e7c7d8e486f80315c2a537671ae71bf7
Illia <ilyusha.drozd.2016@gmail.com>
2024-09-27 13:17:55 +0300

    demo version greet and institution ready

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
-<script></script>
-
 <template>
 	<img class="bg" src="/public/assets/hero.png" alt="" />
 	<header class="k2d-300">
-		<div class="content">
+		<div class="container">
 			Для замовлення та консультацій: +38 (099) 6 800 900; +38 (097) 6 800 900;
 			+38 (073) 6 800 901
 		</div>
 	</header>
-	
-	<main>
-		<section class="content border-gradient">
-			<nav>
+
+	<main class="wrapper">
+		<section class="container border-gradient">
+			<nav class="content">
 				<div class="nav_header">
 					<img
 						src="/public/icons/def-ukr.png	"
@@ -23,24 +21,62 @@
 					</h1>
 				</div>
 				<div class="nav_links">
-					<a href="#">
-						<button class="active-red k2d-700">
-							<img src="/public/icons/check-ico.png" alt="" />
-							Методичні рекомендації
-						</button>
+					<a class="btn active-red k2d-700" href="#">
+						<img src="/public/icons/check-ico.png" alt="" />
+						Методичні рекомендації
 					</a>
-
-					<a href="#">
-						<button class="green k2d-700">Промо</button>
-					</a>
-					<a href="#">
-						<button class="violet k2d-700">Новини</button>
-					</a>
+					<a class="btn green k2d-700" href="#">Промо</a>
+					<a class="btn violet k2d-700" href="#">Quiz</a>
 				</div>
 			</nav>
 		</section>
-		<section class="content border-gradient"><h1>123</h1></section>
+
+		<section
+			style="padding: 2rem 0"
+			class="monda-700 container border-gradient"
+		>
+			<div class="content" v-for="el in institutions" :key="el.name">
+				<a href="">
+					<div class="institutions">
+						<span>{{ el.class }}</span>
+						<hr />
+						<span>{{ el.name }}</span>
+						<hr />
+						<span><img src="/public/icons/orange-book.png" alt="ico" /></span>
+					</div>
+				</a>
+			</div>
+		</section>
 	</main>
 </template>
 
 <style scoped></style>
+
+<script>
+	export default {
+		data: () => ({
+			institutions: [
+				{
+					name: ' Браїлівська гімназія Жмеринської міської територіальної громади',
+					class: '10-A',
+				},
+				{
+					name: 'Філія"Коростівецька гімназія" Комунального закладу "Жмеринський ліцей № 6"',
+					class: '10-A',
+				},
+				{
+					name: 'Філія"Коростівецька гімназія" Комунального закладу "Жмеринський ліцей № 6"',
+					class: '10-A',
+				},
+				{
+					name: 'Філія"Коростівецька гімназія" Комунального закладу "Жмеринський ліцей № 6"',
+					class: '10-A',
+				},
+				{
+					name: 'Філія"Коростівецька гімназія" Комунального закладу "Жмеринський ліцей № 6"',
+					class: '10-В',
+				},
+			],
+		}),
+	}
+</script>
